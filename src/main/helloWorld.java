0ea7aff7deab644c2768eb256438ca4f5994197b
jcoctiza <jcoctiza@gmail.com>
2018-05-08 14:02:04 +0800

    (Second Commit on training-wise)

--- src/main/helloWorld.java
+++ src/main/helloWorld.java
@@ -4,6 +4,7 @@ package main;
  * Created by Tri-Nvent on 5/8/2018.
  */
 public class helloWorld {
+    // This is a test comment. I'm practicing with Git!
     public static void main(String[] args){
         System.out.println("");
     }
