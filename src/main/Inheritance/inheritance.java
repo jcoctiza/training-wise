4447c8d69964325c3e544b907a33ff3e6f6de29b
jcoctiza <jcoctiza@gmail.com>
2018-05-10 13:44:36 +0800

    [Activity 1] Inheritance - Created a class, applied inheritance and created an object.

--- src/main/Inheritance/inheritance.java
+++ src/main/Inheritance/inheritance.java
@@ -1,4 +1,55 @@
 package main.Inheritance;
 
-public class dragon {
+    class Dragon {
+        String name;
+        int legs;
+        String gender;
+        String color;
+        String ability_1;
+        String ability_2;
+        String ability_3;
+
+        public void Dragon() {
+            System.out.println("Name: "+name);
+            System.out.println("Legs: "+legs);
+            System.out.println("Gender: "+gender);
+            System.out.println("Color: "+color);
+            System.out.println("Ability 1: "+ability_1);
+            System.out.println("Ability 2: "+ability_2);
+            System.out.println("Ability 3: "+ability_3);
+            System.out.println("\n");
+        }
+    }
+
+    class RedDragon extends Dragon {
+        RedDragon() {
+            name = "Red Dragon";
+            legs = 4;
+            gender = "Male";
+            color = "Red";
+            ability_1 = "Fly";
+            ability_2 = "Breath Fire";
+            ability_3 = "Invincible";
+        }
+    }
+    class BlueDragon extends Dragon {
+        BlueDragon() {
+        name = "Blue Dragon";
+        legs = 2;
+        gender = "Un-identified";
+        color = "Blue";
+        ability_1 = "Fly";
+        ability_2 = "Breath Ice";
+        ability_3 = "Very Fast";
+    }
 }
+
+    class DragonClass {
+        public static void main(String[] args) {
+            BlueDragon blueDragon = new BlueDragon();
+            RedDragon redDragon = new RedDragon();
+
+            blueDragon.Dragon();
+            redDragon.Dragon();
+        }
+    }
