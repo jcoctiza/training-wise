8b1f88f2e427380b64fd28c0c5bef0bcf8c623ef
jcoctiza <jcoctiza@gmail.com>
2018-05-10 14:41:46 +0800

    [Activity 3] Encapsulation Demo

---
+++ src/main/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,44 @@
+package main.Encapsulation;
+
+class Encapsulation{
+    private String Name;
+    private int Age;
+    private String Color;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+}
+
+public class EncapsulationDemo{
+    public static void main(String args[]){
+        Encapsulation dragon = new Encapsulation();
+        dragon.setName("Red Dragon");
+        dragon.setAge(150);
+        dragon.setColor("Red");
+
+        System.out.println("Dragon Name: " + dragon.getName());
+        System.out.println("Dragon Age: " + dragon.getAge() +" Years Old");
+        System.out.println("Dragon Color: " + dragon.getColor());
+    }
+}
