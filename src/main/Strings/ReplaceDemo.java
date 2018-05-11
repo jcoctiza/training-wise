37c2f42a589b3231c73a4c56b30881652fb1090c
jcoctiza <jcoctiza@gmail.com>
2018-05-11 14:40:29 +0800

    [Activity 6] - String Class: Functions and Methods - String "tolowercase" & Java "touppercase" Method

--- src/main/Strings/ReplaceDemo.java
+++ src/main/Strings/ReplaceDemo.java
@@ -3,15 +3,20 @@ package main.Strings;
 /**
  * Created by Tri-Nvent on 5/11/2018.
  */
-public class EndsWithDemo {
+public class ReplaceDemo {
     public static void main(String[] args){
-        String dragon = "Dragons";
-        String endsWith = "ns";
-        boolean contains = dragon.endsWith(endsWith);
-        if (contains == true) {
-            System.out.println("Yes! The Word Dragons ends with: "+endsWith);
-        }else{
-            System.out.println("Nope! The Word Dragons don't end with: "+endsWith);
-        }
+        String dragon = "Red Dragon";
+        String replace1 = "Bull";
+        String replace2 = "Blue";
+        String replace3 = "Black Bull";
+        String newBeast = dragon.replace("Dragon",replace1);
+        System.out.println(dragon);
+        System.out.println("Dragon Replaced by "+replace1+" = "+newBeast);
+
+        String newDragon = dragon.replaceFirst("Red",replace2);
+        System.out.println("Red Replaced by"+replace2+" = "+newDragon);
+
+        String newName = dragon.replaceAll(dragon,replace3);
+        System.out.println(dragon+" Replaced by "+replace3+" = "+newName);
     }
 }
