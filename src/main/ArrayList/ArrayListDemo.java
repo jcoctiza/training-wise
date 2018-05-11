88da6e9e8d76c19d60aec5c9884d8fa7b7e29aeb
jcoctiza <jcoctiza@gmail.com>
2018-05-11 09:59:58 +0800

    [Activity 5] - Array of Object and Array List Demo

--- src/main/ArrayList/ArrayListDemo.java
+++ src/main/ArrayList/ArrayListDemo.java
@@ -1,7 +1,44 @@
 package main.ArrayList;
 
+import java.util.ArrayList;
+
 /**
  * Created by Tri-Nvent on 5/11/2018.
  */
 public class ArrayListDemo {
+    public static void main(String[] args){
+        //Creating a generic ArrayList
+        ArrayList dragon = new ArrayList();
+        //Size of arrayList
+        System.out.println("Size of ArrayList at creation: " + dragon.size());
+        //Lets add some elements to it
+        dragon.add("Blue Dragon");
+        dragon.add("Red Dragon");
+        dragon.add("Black Dragon");
+
+        //Recheck the size after adding elements
+        System.out.println("Size of ArrayList after adding elements: " + dragon.size());
+
+        //Display all contents of ArrayList
+        System.out.println("List of all elements: " + dragon);
+
+        //Remove some elements from the list
+        dragon.remove("");
+        System.out.println("See contents after removing one element: " + dragon);
+
+        //Remove element by index
+        dragon.remove(2);
+        System.out.println("See contents after removing element by index: " + dragon);
+
+        //Check size after removing elements
+        System.out.println("Size of arrayList after removing elements: " + dragon.size());
+        System.out.println("List of all elements after removing elements: " + dragon);
+
+        //Check if the list contains "K"
+        if (dragon.contains("Red Dragon") == true) {
+            System.out.println("Red Dragon Exists");
+        }
+    }
+
 }
+
--- src/main/ArrayOfObject/ArrayOfObjectDemo.java
+++ src/main/ArrayOfObject/ArrayOfObjectDemo.java
@@ -4,4 +4,28 @@ package main.ArrayOfObject;
  * Created by Tri-Nvent on 5/11/2018.
  */
 public class ArrayOfObjectDemo {
+    public static void main(String[] args){
+        Dragon dragonz[] = new Dragon[2] ;
+        dragonz[0] = new Dragon();
+        dragonz[1] = new Dragon();
+        dragonz[0].setData(2,4);
+        dragonz[1].setData(4,2);
+        System.out.println("For Array Element: 0");
+        dragonz[0].showData();
+        System.out.println("\n");
+        System.out.println("For Array Element: 1");
+        dragonz[1].showData();
+    }
+}
+class Dragon{
+    int leg;
+    int wing;
+    public void setData(int legs,int wings){
+        leg=legs;
+        wing=wings;
+    }
+    public void showData(){
+        System.out.println("Dragon's Leg ="+leg);
+        System.out.println("Dragon's Wing ="+wing);
+    }
 }
