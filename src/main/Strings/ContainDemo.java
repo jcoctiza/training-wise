b153c3dfb210e405385c71417c9e5eba8972291b
jcoctiza <jcoctiza@gmail.com>
2018-05-11 14:24:33 +0800

    [Activity 6] - String Class: Functions and Methods - String "endsWith" Method

--- src/main/Strings/ContainDemo.java
+++ src/main/Strings/ContainDemo.java
@@ -4,4 +4,14 @@ package main.Strings;
  * Created by Tri-Nvent on 5/11/2018.
  */
 public class ContainDemo {
+    public static void main(String[] args){
+        String dragon = "Dragons";
+        String a = "aaa";
+        boolean contains = dragon.contains(a);
+        if (contains == true) {
+            System.out.println("Yes! The Word Dragons Contains: "+a);
+        }else{
+            System.out.println("Nah! The Word Dragons don't have: "+a);
+        }
+    }
 }
