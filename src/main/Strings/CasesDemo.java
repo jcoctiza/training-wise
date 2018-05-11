2f36f3e44a647fa595fe628dfe3defb21ce2ff54
jcoctiza <jcoctiza@gmail.com>
2018-05-11 14:46:59 +0800

    [Activity 6] - String Class: Functions and Methods - String "tolowercase" & Java "touppercase" Method

---
+++ src/main/Strings/CasesDemo.java
@@ -0,0 +1,22 @@
+package main.Strings;
+
+/**
+ * Created by Tri-Nvent on 5/11/2018.
+ */
+public class ReplaceDemo {
+    public static void main(String[] args){
+        String dragon = "Red Dragon";
+        String replace1 = "Bull";
+        String replace2 = "Blue";
+        String replace3 = "Black Bull";
+        String newBeast = dragon.replace("Dragon",replace1);
+        System.out.println(dragon);
+        System.out.println("Dragon Replaced by "+replace1+" = "+newBeast);
+
+        String newDragon = dragon.replaceFirst("Red",replace2);
+        System.out.println("Red Replaced by"+replace2+" = "+newDragon);
+
+        String newName = dragon.replaceAll(dragon,replace3);
+        System.out.println(dragon+" Replaced by "+replace3+" = "+newName);
+    }
+}
