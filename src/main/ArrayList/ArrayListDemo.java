e39d7c41cf8e4ca0d567ec4ab4bea69aec037aac
jcoctiza <jcoctiza@gmail.com>
2018-05-11 10:06:24 +0800

    [Activity 5] - Array of Object and Array List Demo

--- src/main/ArrayList/ArrayListDemo.java
+++ src/main/ArrayList/ArrayListDemo.java
@@ -7,34 +7,22 @@ import java.util.ArrayList;
  */
 public class ArrayListDemo {
     public static void main(String[] args){
-        //Creating a generic ArrayList
         ArrayList dragon = new ArrayList();
-        //Size of arrayList
-        System.out.println("Size of ArrayList at creation: " + dragon.size());
-        //Lets add some elements to it
+        System.out.println("Size of ArrayList upon creation: " + dragon.size());
+
         dragon.add("Blue Dragon");
         dragon.add("Red Dragon");
         dragon.add("Black Dragon");
 
-        //Recheck the size after adding elements
         System.out.println("Size of ArrayList after adding elements: " + dragon.size());
-
-        //Display all contents of ArrayList
         System.out.println("List of all elements: " + dragon);
 
-        //Remove some elements from the list
-        dragon.remove("");
-        System.out.println("See contents after removing one element: " + dragon);
-
-        //Remove element by index
         dragon.remove(2);
         System.out.println("See contents after removing element by index: " + dragon);
 
-        //Check size after removing elements
         System.out.println("Size of arrayList after removing elements: " + dragon.size());
         System.out.println("List of all elements after removing elements: " + dragon);
 
-        //Check if the list contains "K"
         if (dragon.contains("Red Dragon") == true) {
             System.out.println("Red Dragon Exists");
         }
