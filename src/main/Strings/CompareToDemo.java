834d59405587a50ce7625d0966e65ba5f5f2935a
jcoctiza <jcoctiza@gmail.com>
2018-05-11 14:12:41 +0800

    [Activity 6] - String Class: Functions and Methods - String "compareTo" Method

---
+++ src/main/Strings/CompareToDemo.java
@@ -0,0 +1,23 @@
+package main.Strings;
+
+/**
+ * Created by Tri-Nvent on 5/11/2018.
+ */
+public class CompareToDemo {
+        public static void main(String[] args){
+            String dragon = "Dragons";
+
+            int compare = dragon.compareTo("dragons");
+            if(compare == 0){
+                System.out.println("Comparing: Matched!");
+            }else{
+                System.out.println("Comparing: Didn't Matched!");
+            }
+            int compareIgnoreCase = dragon.compareToIgnoreCase("dRaGoNs");
+            if(compareIgnoreCase == 0){
+            System.out.println("Ignore Case Comparing: Matched!");
+            }else{
+                System.out.println("Ignore Case Comparing: Didn't Matched!");
+            }
+    }
+}
--- src/main/Strings/IndexOfDemo.java
+++ src/main/Strings/IndexOfDemo.java
@@ -4,4 +4,9 @@ package main.Strings;
  * Created by Tri-Nvent on 5/11/2018.
  */
 public class IndexOfDemo {
+        public static void main(String[] args){
+            String dragon = "Dragons";
+            System.out.println("Character at position 5: " + dragon.charAt(5));
+            System.out.println("Index of character 'r': " + dragon.indexOf('r'));
+        }
 }
