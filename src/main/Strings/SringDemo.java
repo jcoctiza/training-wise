ac0af1d1f01f2214ee3347fc6df0a40d040db981
jcoctiza <jcoctiza@gmail.com>
2018-05-11 10:47:02 +0800

    [Activity 6] - String Class: Functions and Methods

---
+++ src/main/Strings/SringDemo.java
@@ -0,0 +1,17 @@
+package main.Strings;
+
+/**
+ * Created by Tri-Nvent on 5/11/2018.
+ */
+public class SringsDemo {
+    public static void main(String[] args){
+        String firstName = "James Carlo";
+        String lastName = "Octiza";
+        System.out.println("String Concatenation:");
+        String fullName = firstName.concat(" "+lastName);
+        System.out.println(fullName);
+        System.out.println("\nUsing '+' Operator:");
+        String FullName = firstName + lastName;
+        System.out.println(FullName);
+    }
+}
