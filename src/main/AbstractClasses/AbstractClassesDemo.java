d0295572dae9b9875e854320c4525b34bf0235d3
jcoctiza <jcoctiza@gmail.com>
2018-05-15 18:06:31 +0800

    [Activity 11] Abstract Method and Class Demo

--- src/main/AbstractClasses/AbstractClassesDemo.java
+++ src/main/AbstractClasses/AbstractClassesDemo.java
@@ -1,7 +1,24 @@
 package main.AbstractClasses;
-
-/**
- * Created by Tri-Nvent on 5/15/2018.
- */
 public class AbstractClassesDemo {
+    public static void main(String[] args){
+        RedDragon rd = new RedDragon();
+        rd.power();
+        BlueDragon bd = new BlueDragon();
+        bd.power();
+    }
+}
+abstract class Dragon{
+    int wings = 2;
+    int legs = 4;
+    abstract public void power();
+}
+class RedDragon extends Dragon{
+    public void power(){
+        System.out.println("Breath Fire!");
+    }
+}
+class BlueDragon extends Dragon{
+    public void power(){
+        System.out.println("Breath Ice!");
+    }
 }
