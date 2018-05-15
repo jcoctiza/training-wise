67c1eb82fb450e6c55b0f853604447a295bba086
jcoctiza <jcoctiza@gmail.com>
2018-05-15 18:31:08 +0800

    [Activity 13] Java Constructors Demo

--- src/main/JavaConstructors/JavaConstractorsDemo.java
+++ src/main/JavaConstructors/JavaConstractorsDemo.java
@@ -3,5 +3,23 @@ package main.JavaConstructors;
 /**
  * Created by Tri-Nvent on 5/15/2018.
  */
-public class JavaContractorsDemo {
+public class JavaConstractorsDemo {
+      private String dragon1;
+      private String dragon2;
+
+    JavaConstractorsDemo(){
+        dragon1 = "Red Dragon";
+        dragon2 = "Blue dragon";
+        System.out.println("Dragons Inside the Constructor:");
+    }
+
+    public void showDragons(){
+          System.out.println("Dragon 1 = "+dragon1);
+          System.out.println("Dragon 2 = "+dragon2);
+    }
+
+    public static void main(String[] args){
+        JavaConstractorsDemo dragons = new JavaConstractorsDemo();
+        dragons.showDragons();
+    }
 }
