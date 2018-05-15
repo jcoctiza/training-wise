b805ac6283f4e9995f86d288f5ce9a88185fa575
jcoctiza <jcoctiza@gmail.com>
2018-05-15 13:41:42 +0800

    [Activity 9] Static Method and Variable

---
+++ src/main/StaticMethodAndVariable/StaticMethodAndVariableDemo.java
@@ -0,0 +1,26 @@
+package main.GarbageCollection;
+
+public class GarbageCollectionDemo{
+    public static void main(String args[]){
+        Student s1 = new Student();
+        s1.showData();
+        Student s2 = new Student();
+        s2.showData();
+        Student.b++;
+        s1.showData();
+    }
+}
+
+class Student {
+    int a;
+    static int b;
+
+    Student(){
+        b++;
+    }
+
+    public void showData(){
+        System.out.println("Value of a = "+a);
+        System.out.println("Value of b = "+b);
+    }
+}
