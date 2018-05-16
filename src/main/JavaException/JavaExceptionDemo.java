d5c72813411f12d9cf72d44adbbcf49729c0391e
jcoctiza <jcoctiza@gmail.com>
2018-05-16 11:04:18 +0800

    [Activity 14] Exception Handling - Arrays and Arithmetic Operation

--- src/main/JavaException/JavaExceptionDemo.java
+++ src/main/JavaException/JavaExceptionDemo.java
@@ -1,7 +1,33 @@
 package main.JavaException;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by Tri-Nvent on 5/16/2018.
  */
 public class JavaExceptionDemo {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 20;
+        String array[ ] = {"James", "Carlo", "Octiza"};
+        System.out.println("For Arithmetic Operation:");
+        try {
+            int c = b/a;
+            System.out.println(c);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Something went wrong due to "+ e);
+        }
+
+        System.out.println("\nFor Arrays:");
+        try {
+            System.out.println(array[3]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Something went wrong due to "+ e);
+        }
+        finally{
+            System.out.println("\nJava Exception Demo");
+        }
+    }
 }
