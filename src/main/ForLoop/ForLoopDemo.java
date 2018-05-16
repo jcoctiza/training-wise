45338a5e1e0153b0970a26e510c7693a6cb51f0f
jcoctiza <jcoctiza@gmail.com>
2018-05-16 11:48:34 +0800

    [Activity 15] For Loop - using ForEach

--- src/main/ForLoop/ForLoopDemo.java
+++ src/main/ForLoop/ForLoopDemo.java
@@ -4,4 +4,11 @@ package main.ForLoop;
  * Created by Tri-Nvent on 5/16/2018.
  */
 public class ForLoopDemo {
+    public static void main(String[] args){
+        String dragon[] = {"Red Dragon", "Blue Dragon", "Black Dragon"};
+        System.out.println("List of Dragons using forLoop:");
+        for(String dragons: dragon){
+            System.out.println(dragons);
+        }
+    }
 }
