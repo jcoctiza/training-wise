76f259ebac9a01ba5ded80f6bea78dc242694b5c
jcoctiza <jcoctiza@gmail.com>
2018-05-17 11:29:55 +0800

    [Activity 18] Maths with Java - Math.round method - added Scanner class for user input.

--- src/main/mathsWithJava/RoundMethodDemo.java
+++ src/main/mathsWithJava/RoundMethodDemo.java
@@ -2,27 +2,10 @@ package main.mathsWithJava;
 
 import java.util.Scanner;
 
-public class AbsMethodDemo {
+public class RoundMethodDemo {
     public static void main(String args[]){
-        System.out.println("Type 1 for Int input || Type 2 for Double input");
+        System.out.println("Input a decimal number:");
         Scanner input = new Scanner(System.in);
-
-        switch(input.nextInt()){
-            case 1:
-                Scanner intInput = new Scanner(System.in);
-                System.out.println("Int: ");
-                System.out.println("Absolute value: " +Math.abs(intInput.nextInt()));
-                break;
-
-            case 2:
-                Scanner doubleInput = new Scanner(System.in);
-                System.out.println("Double: ");
-                System.out.println("Absolute value: " +Math.abs(doubleInput.nextDouble()));
-                break;
-
-            default:
-                System.out.println("Please read the instruction!");
-                break;
-        }
+        System.out.print("Round off to: "+Math.round(input.nextDouble()));
     }
 }
