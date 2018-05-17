d98a0fd9ec5e8755ac6b4b0d8f20056cb98702ae
jcoctiza <jcoctiza@gmail.com>
2018-05-17 11:21:30 +0800

    [Activity 18] Maths with Java - Math.abs method - added Scanner class for user input and Switch statement for int or double datatype inputs

--- src/main/mathsWithJava/AbsMethodDemo.java
+++ src/main/mathsWithJava/AbsMethodDemo.java
@@ -1,7 +1,28 @@
 package main.mathsWithJava;
 
-/**
- * Created by Tri-Nvent on 5/17/2018.
- */
+import java.util.Scanner;
+
 public class AbsMethodDemo {
+    public static void main(String args[]){
+        System.out.println("Type 1 for Int input || Type 2 for Double input");
+        Scanner input = new Scanner(System.in);
+
+        switch(input.nextInt()){
+            case 1:
+                Scanner intInput = new Scanner(System.in);
+                System.out.println("Int: ");
+                System.out.println("Absolute value: " +Math.abs(intInput.nextInt()));
+                break;
+
+            case 2:
+                Scanner doubleInput = new Scanner(System.in);
+                System.out.println("Double: ");
+                System.out.println("Absolute value: " +Math.abs(doubleInput.nextDouble()));
+                break;
+
+            default:
+                System.out.println("Please read the instruction!");
+                break;
+        }
+    }
 }
