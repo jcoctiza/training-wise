7a6bbfa7359f79e47c8847861c16e878df06f77f
jcoctiza <jcoctiza@gmail.com>
2018-05-17 10:48:57 +0800

    [Activity 17] Throws Keyword Demo

--- src/main/throwsKeyword/ThrowsKeywordDemo.java
+++ src/main/throwsKeyword/ThrowsKeywordDemo.java
@@ -4,4 +4,16 @@ package main.throwsKeyword;
  * Created by Tri-Nvent on 5/17/2018.
  */
 public class ThrowsKeywordDemo {
+
+    public static void main(String args[]) throws ArithmeticException{
+        int a, b, c;
+        a = 0;
+        b = 20;
+        try{
+            c = b/a;
+            System.out.print(c);
+        }catch(ArithmeticException e){
+            System.out.print("We do not divide any numbers to ZERO!");
+        }
+    }
 }
