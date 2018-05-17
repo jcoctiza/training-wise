bbc3769b7878ae752aa6b80ae213076960016ef4
jcoctiza <jcoctiza@gmail.com>
2018-05-17 13:32:16 +0800

    [Activity 18] Maths with Java - Exponential and Logarithmic Method Demo - Math.exp, Math.log, Math.pow and Math.sqrt method - added Scanner class for user input and Switch statement for method selection.

--- src/main/mathsWithJava/ExponentialAndLogarithmicMethodDemo.java
+++ src/main/mathsWithJava/ExponentialAndLogarithmicMethodDemo.java
@@ -2,34 +2,52 @@ package main.mathsWithJava;
 
 import java.util.Scanner;
 
-public class MinAndMaxMethodDemo {
+public class ExponentialAndLogarithmicMethodDemo {
     public static void main(String args[]){
-        System.out.println("Type 1 for Int input || Type 2 for Double input");
+        System.out.println("Please Select a Method:");
+        System.out.println("[1] exp()");
+        System.out.println("[2] log()");
+        System.out.println("[3] pow()");
+        System.out.println("[4] sqrt()");
+        System.out.print("\nMethod: ");
         Scanner input = new Scanner(System.in);
-
         switch(input.nextInt()){
             case 1:
-                Scanner intInput = new Scanner(System.in);
-                System.out.print("Int 1: ");
-                int intNum1 = intInput.nextInt();
-                System.out.print("Int 2: ");
-                int intNum2 = intInput.nextInt();
-                System.out.println("Minimum out of "+intNum1+" and "+intNum2+" is "+Math.min(intNum1,intNum2));
-                System.out.println("Maximum out of "+intNum1+" and "+intNum2+" is "+Math.max(intNum1,intNum2));
+                System.out.println("You have selected Exp method");
+                System.out.print("Input a decimal number:  ");
+                Scanner expInput = new Scanner(System.in);
+                double expNum = expInput.nextDouble();
+                System.out.println("exp("+expNum+") = "+Math.exp(expNum));
                 break;
 
             case 2:
-                Scanner doubleInput = new Scanner(System.in);
-                System.out.print("Double 1: ");
-                double doubleNum1 = doubleInput.nextDouble();
-                System.out.print("Double 2: ");
-                double doubleNum2 = doubleInput.nextDouble();
-                System.out.println("Minimum out of "+doubleNum1+" and "+doubleNum2+" is "+Math.min(doubleNum1,doubleNum2));
-                System.out.println("Maximum out of "+doubleNum1+" and "+doubleNum2+" is "+Math.max(doubleNum1,doubleNum2));
+                System.out.println("You have selected Log method");
+                System.out.print("Input a decimal number:  ");
+                Scanner logInput = new Scanner(System.in);
+                double logNum = logInput.nextDouble();
+                System.out.println("log("+logNum+") = "+Math.log(logNum));
+                break;
+
+            case 3:
+                System.out.println("You have selected Pow method");
+                Scanner powInput = new Scanner(System.in);
+                System.out.print("Input a Number:  ");
+                int powNum1 = powInput.nextInt();
+                System.out.print("Input an Exponent:  ");
+                int powNum2 = powInput.nextInt();
+                System.out.println(powNum1+" raised to the power of "+powNum2+" = "+Math.pow(powNum1,powNum2));
+                break;
+
+            case 4:
+                System.out.println("You have selected Log method");
+                System.out.print("Input a number:  ");
+                Scanner sqrtInput = new Scanner(System.in);
+                int sqrtNum = sqrtInput.nextInt();
+                System.out.println("The square root of "+sqrtNum+" = "+Math.sqrt(sqrtNum));
                 break;
 
             default:
-                System.out.println("Please read the instruction!");
+                System.out.println("Please read the instruction and try again!");
                 break;
         }
     }
