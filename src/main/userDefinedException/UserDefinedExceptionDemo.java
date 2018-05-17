f669df8fface458a2449015ef554523083081e62
jcoctiza <jcoctiza@gmail.com>
2018-05-17 10:12:37 +0800

    [Activity 16] Java Exception - User Defined Exception

--- src/main/userDefinedException/UserDefinedExceptionDemo.java
+++ src/main/userDefinedException/UserDefinedExceptionDemo.java
@@ -4,4 +4,20 @@ package main.userDefinedException;
  * Created by Tri-Nvent on 5/17/2018.
  */
 public class UserDefinedExceptionDemo {
+    public static void main(String args[]) {
+        try {
+            throw new MyException("This is my Exception!");
+        } catch (MyException e) {
+            System.out.println(e);
+        }
+    }
+}
+class MyException extends Exception{
+    String exception;
+    MyException(String myException){
+        exception = myException;
+    }
+    public String toString(){
+        return (exception) ;
+    }
 }
