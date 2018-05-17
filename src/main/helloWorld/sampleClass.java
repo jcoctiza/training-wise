9ced174001eaf853e2c2eaca69ae9f3a3baa5d9e
jcoctiza <jcoctiza@gmail.com>
2018-05-17 19:32:08 +0800

    Sample Commit 1

--- src/main/helloWorld/sampleClass.java
+++ src/main/helloWorld/sampleClass.java
@@ -4,4 +4,7 @@ package main.helloWorld;
  * Created by Tri-Nvent on 5/17/2018.
  */
 public class sampleClass {
+    public static void main(String[] args){
+        System.out.println("Hi World!");
+    }
 }
